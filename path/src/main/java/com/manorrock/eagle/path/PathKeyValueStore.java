fa63885318bab6c9bf9a8b186a844d275208d807
Manfred Riem <22894110+manorrock@users.noreply.github.com>
2021-02-25 06:48:40 -0700

    Fixes issue #15 - Provide implementation for Path module

---
+++ path/src/main/java/com/manorrock/eagle/path/PathKeyValueStore.java
@@ -0,0 +1,130 @@
+/*
+ *  Copyright (c) 2002-2021, Manorrock.com. All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *      1. Redistributions of source code must retain the above copyright
+ *         notice, this list of conditions and the following disclaimer.
+ *
+ *      2. Redistributions in binary form must reproduce the above copyright
+ *         notice, this list of conditions and the following disclaimer in the
+ *         documentation and/or other materials provided with the distribution.
+ *
+ *      3. Neither the name of the copyright holder nor the names of its 
+ *         contributors may be used to endorse or promote products derived from
+ *         this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.manorrock.eagle.path;
+
+import com.manorrock.eagle.common.IdentityMapper;
+import com.manorrock.eagle.api.KeyValueMapper;
+import com.manorrock.eagle.api.KeyValueStore;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import static java.util.logging.Level.WARNING;
+import java.util.logging.Logger;
+
+/**
+ * A path based KeyValueStore.
+ *
+ * @author Manfred Riem (mriem@manorrock.com)
+ * @param <K> the type of the key.
+ * @param <V> the type of the value.
+ */
+public class PathKeyValueStore<K, V> implements KeyValueStore<K, V> {
+
+    /**
+     * Stores the logger.
+     */
+    private static final Logger LOGGER
+            = Logger.getLogger(PathKeyValueStore.class.getPackage().getName());
+
+    /**
+     * Stores the key mapper.
+     */
+    private KeyValueMapper keyMapper;
+
+    /**
+     * Stores the value mapper.
+     */
+    private KeyValueMapper valueMapper;
+
+    /**
+     * Constructor.
+     *
+     * @param basePath the base path.
+     */
+    public PathKeyValueStore(Path basePath) {
+        this.keyMapper = new FilenameToPathMapper(basePath);
+        this.valueMapper = new IdentityMapper();
+    }
+
+    @Override
+    public void delete(K key) {
+        try {
+            Path path = (Path) keyMapper.to(key);
+            Files.deleteIfExists(path);
+        } catch (IOException ioe) {
+            LOGGER.log(WARNING, "An I/O error occured deleting key: " + key, ioe);
+        }
+    }
+
+    @Override
+    public V get(K key) {
+        V result = null;
+        Path path = (Path) keyMapper.to(key);
+        if (Files.exists(path)) {
+            try {
+                byte[] bytes = Files.readAllBytes(path);
+                result = (V) valueMapper.from(bytes);
+            } catch (IOException ioe) {
+                LOGGER.log(WARNING, "Unable to get content for key: " + key, ioe);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        Path path = (Path) keyMapper.to(key);
+        if (!Files.exists(path.getParent())) {
+            try {
+                Files.createDirectories(path.getParent());
+            } catch (IOException ioe) {
+                LOGGER.log(WARNING, "Unable to create directories for key: " + key, ioe);
+                return;
+            }
+        }
+        try (OutputStream output = Files.newOutputStream(path)) {
+            output.write((byte[]) valueMapper.to(value));
+            output.flush();
+        } catch (IOException ioe) {
+            LOGGER.log(WARNING, "Unable to put content for key: " + key, ioe);
+        }
+    }
+
+    @Override
+    public void setKeyMapper(KeyValueMapper keyMapper) {
+        this.keyMapper = keyMapper;
+    }
+
+    @Override
+    public void setValueMapper(KeyValueMapper valueMapper) {
+        this.valueMapper = valueMapper;
+    }
+}
---
+++ path/src/test/java/com/manorrock/eagle/path/PathKeyValueStoreTest.java
@@ -0,0 +1,110 @@
+/*
+ *  Copyright (c) 2002-2021, Manorrock.com. All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *      1. Redistributions of source code must retain the above copyright
+ *         notice, this list of conditions and the following disclaimer.
+ *
+ *      2. Redistributions in binary form must reproduce the above copyright
+ *         notice, this list of conditions and the following disclaimer in the
+ *         documentation and/or other materials provided with the distribution.
+ *
+ *      3. Neither the name of the copyright holder nor the names of its 
+ *         contributors may be used to endorse or promote products derived from
+ *         this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.manorrock.eagle.path;
+
+import com.manorrock.eagle.common.StringToByteArrayMapper;
+import java.io.File;
+import java.nio.file.Path;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The JUnit tests for the FilesystemKeyValueStore class.
+ *
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class PathKeyValueStoreTest {
+
+    /**
+     * Test delete method.
+     */
+    @Test
+    public void testDelete() {
+        PathKeyValueStore<String, byte[]> kvs
+                = new PathKeyValueStore<>(Path.of("target"));
+        kvs.put("delete", "deleteme".getBytes());
+        assertTrue(new File("target/delete").exists());
+        kvs.delete("delete");
+        assertFalse(new File("target/delete").exists());
+    }
+    
+    /**
+     * Test delete method.
+     */
+    @Test
+    public void testDelete2() {
+        PathKeyValueStore<String, String> kvs
+                = new PathKeyValueStore<>(Path.of("target"));
+        kvs.setValueMapper(new StringToByteArrayMapper());
+        kvs.put("delete", "deleteme");
+        assertTrue(new File("target/delete").exists());
+        kvs.delete("delete");
+        assertFalse(new File("target/delete").exists());
+    }
+    
+    /**
+     * Test get method.
+     */
+    @Test
+    public void testGet() {
+        PathKeyValueStore<String, byte[]> kvs
+                = new PathKeyValueStore<>(Path.of("target"));
+        kvs.put("get", "getme".getBytes());
+        assertTrue(new File("target/get").exists());
+        assertEquals("getme", new String(kvs.get("get")));
+    }
+    
+    /**
+     * Test get method.
+     */
+    @Test
+    public void testGet2() {
+        PathKeyValueStore<String, String> kvs
+                = new PathKeyValueStore<>(Path.of("target"));
+        kvs.setValueMapper(new StringToByteArrayMapper());
+        kvs.put("get", "getme");
+        assertTrue(new File("target/get").exists());
+        assertEquals("getme", kvs.get("get"));
+    }
+
+    /**
+     * Test put method.
+     */
+    @Test
+    public void testPut() {
+        PathKeyValueStore<String, byte[]> kvs
+                = new PathKeyValueStore<>(Path.of("target"));
+        kvs.put("put", "putme".getBytes());
+        assertTrue(new File("target/put").exists());
+        assertEquals("putme", new String(kvs.get("put")));
+    }
+}
