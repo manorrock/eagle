3e697f4745cb37356587d4e47f3216a4c8bd6f7f
Manfred Riem <22894110+manorrock@users.noreply.github.com>
2021-02-27 08:04:28 -0700

    Fixes issue #20 - Add module-info files

--- redis/src/test/java/com/manorrock/eagle/redis/RedisKeyValueStoreTest.java
+++ redis/src/test/java/com/manorrock/eagle/redis/RedisKeyValueStoreTest.java
@@ -27,9 +27,8 @@
  *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  */
-package com.manorrock.common.kvs.redis;
+package com.manorrock.eagle.redis;
 
-import com.manorrock.eagle.redis.RedisKeyValueStore;
 import io.lettuce.core.RedisURI;
 import java.io.FileInputStream;
 import java.io.IOException;
