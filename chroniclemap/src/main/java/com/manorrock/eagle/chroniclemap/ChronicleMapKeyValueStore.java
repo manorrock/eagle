ee3b3406a4e13e2ff1552d36cc0ffda8625984a2
Manfred Riem <22894110+manorrock@users.noreply.github.com>
2021-03-09 16:11:12 -0700

    Fixes issue #21 - Add ChronicleMap implementation

---
+++ chroniclemap/src/main/java/com/manorrock/eagle/chroniclemap/ChronicleMapKeyValueStore.java
@@ -0,0 +1,99 @@
+/*
+ *  Copyright (c) 2002-2021, Manorrock.com. All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *      1. Redistributions of source code must retain the above copyright
+ *         notice, this list of conditions and the following disclaimer.
+ *
+ *      2. Redistributions in binary form must reproduce the above copyright
+ *         notice, this list of conditions and the following disclaimer in the
+ *         documentation and/or other materials provided with the distribution.
+ *
+ *      3. Neither the name of the copyright holder nor the names of its 
+ *         contributors may be used to endorse or promote products derived from
+ *         this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.manorrock.eagle.chroniclemap;
+
+import com.manorrock.eagle.api.KeyValueStore;
+import com.manorrock.eagle.api.KeyValueStoreMapper;
+import com.manorrock.eagle.common.IdentityMapper;
+import net.openhft.chronicle.map.ChronicleMap;
+import net.openhft.chronicle.map.ChronicleMapBuilder;
+
+/**
+ * A ChronicleMap based KeyValueStore.
+ *
+ * @author Manfred Riem (mriem@manorrock.com)
+ * @param <K> the type of the key.
+ * @param <V> the type of the value.
+ */
+public class ChronicleMapKeyValueStore<K, V> implements KeyValueStore<K, V> {
+    
+    /**
+     * Stores the ChronicleMap.
+     */
+    private final ChronicleMap<byte[], byte[]> chronicleMap;
+
+    /**
+     * Stores the key mapper.
+     */
+    private KeyValueStoreMapper keyMapper;
+
+    /**
+     * Stores the value mapper.
+     */
+    private KeyValueStoreMapper valueMapper;
+
+    /**
+     * Constructor.
+     *
+     * @param name the name.
+     */
+    public ChronicleMapKeyValueStore(String name) {
+        chronicleMap = ChronicleMapBuilder
+                .of(byte[].class, byte[].class)
+                .create();
+        keyMapper = new IdentityMapper();
+        valueMapper = new IdentityMapper();
+    }
+
+    @Override
+    public void delete(K key) {
+        chronicleMap.remove((byte[]) keyMapper.to(key));
+    }
+
+    @Override
+    public V get(K key) {
+        return (V) valueMapper.from((byte[]) chronicleMap.get((byte[]) keyMapper.to(key)));
+    }
+
+    @Override
+    public void put(K key, V value) {
+        chronicleMap.put((byte[]) keyMapper.to(key), (byte[]) valueMapper.to(value));
+    }
+
+    @Override
+    public void setKeyMapper(KeyValueStoreMapper<K, ?> keyMapper) {
+        this.keyMapper = keyMapper;
+    }
+
+    @Override
+    public void setValueMapper(KeyValueStoreMapper<V, ?> valueMapper) {
+        this.valueMapper = valueMapper;
+    }
+}
