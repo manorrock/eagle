01a966ec5f86a3c5b8e3e1f331d250640c4c3110
Manfred Riem <mriem@manorrock.com>
2023-03-14 14:24:40 -0500

    Fixes issue #114 - Deprecate Azure Blob Storage implementation for removal

--- azure-blob/src/main/java/com/manorrock/eagle/azure/blob/BlobKeyValueStore.java
+++ azure-blob/src/main/java/com/manorrock/eagle/azure/blob/BlobKeyValueStore.java
@@ -48,7 +48,9 @@ import com.manorrock.eagle.api.KeyValueStoreMapper;
  * @author Manfred Riem (mriem@manorrock.com)
  * @param <K> the type of the key.
  * @param <V> the type of the value.
+ * @deprecated 
  */
+@Deprecated(since = "23.4.0", forRemoval = true)
 public class BlobKeyValueStore<K, V> implements KeyValueStore<K, V, BlobKeyValueStoreMapper> {
     
     /**
