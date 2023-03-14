1b15f7018ef847ae90e9289b8c73dadfa3f3294f
Manfred Riem <mriem@manorrock.com>
2023-03-14 10:04:51 -0500

    Fixes issue #107 - Deprecate Redis implementation for removal

--- redis/src/main/java/com/manorrock/eagle/redis/RedisKeyValueStoreMapper.java
+++ redis/src/main/java/com/manorrock/eagle/redis/RedisKeyValueStoreMapper.java
@@ -6,7 +6,9 @@ import com.manorrock.eagle.api.KeyValueStoreMapper;
  * The Redis KeyValueStoreMapper.
  * 
  * @author Manfred Riem (mriem@manorrock.com)
+ * @deprecated 
  */
+@Deprecated(since = "23.4.0", forRemoval = true)
 public class RedisKeyValueStoreMapper implements KeyValueStoreMapper<String, byte[]> {
     
     @Override
